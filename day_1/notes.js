8626f2cf9ee65d243bf29497b645eafb677b3153
Mohammed Bakhar <mrbakhar8686@gmail.com>
2026-06-06 18:18:30 +0530

    notes done

---
+++ day_1/notes.js
@@ -0,0 +1,33 @@
+// let age = 23;
+// let name = "Tony Stark";
+// let isStudent = true;
+
+
+// let score;
+// console.log(score)
+
+// score = 95;
+// console.log(score)
+
+// let age = 23; //integer
+// let price = 99.99; //decimal number 
+// let temperature = -15; //negative number
+// let distance = 1.5e6; //scientific notation(1,500,000)
+
+
+// //string
+
+// let name = "Tony Stark"; // double quotes
+// let greeting = 'Hello World'; // single quotes
+// let message = `${name}`; // Template Literals (backticks)
+
+// let age = 23
+// let info = `i am ${age} years old`;
+
+let isStudent = true;
+let isLoggedIn = false;
+let hasPermission = true;
+
+if(isStudent){
+    console.log("Student discount applied")
+}
