da99522305a8f5017ce13866ab9799ca974b58e0
Mohammed Bakhar <mrbakhar8686@gmail.com>
2026-06-08 18:55:23 +0530

    assignment day-1

---
+++ day_1/assignment.js
@@ -0,0 +1,104 @@
+// ASSIGNMENT
+
+
+// Problem-1
+
+// let name = "Mohammed Bakhar";
+// let age = 19;
+// let isStudent = true;
+// let favoriteColor = "yellow";
+// let siblings = 3;
+
+
+//Problem-2
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof favoriteColor);
+// console.log(typeof siblings);
+
+//Problem-3
+
+// let score;
+// console.log(score);
+// console.log(typeof score);
+// score = null;
+// console.log(score);
+// console.log(typeof score);
+
+// undefined is when we forget to assign value 
+// null is when we intensionally not assign value
+
+//Problem-4
+
+// let firstName = "Mohammed";
+// let lastName = "Bakhar";
+// fullName = firstName + lastName;
+// console.log(fullName);
+
+
+//Problem-5
+
+// let a = "5";
+// let b = 3 ;
+// let result = a + b;
+// console.log(result);
+// console.log(typeof result);
+
+// as we write 5 in ""(double course) so it assume that 5 is string so it keep it side by side and because 1 is string it make result type as string
+
+// part B
+
+
+//Problem-6
+
+// let num1 = 45;
+// let num2 = 12;
+// console.log(num1+num2);  
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+
+
+//Problem-7
+
+
+// let celsius = 25;
+// let fahrenheit = (celsius * 9/5) + 32
+// console.log(fahrenheit);
+
+
+//Problem-8
+
+
+// let radius = 7;
+// let circumference = 2 * Math.PI * radius;
+// let area = Math.PI * radius * radius;
+
+// console.log("Radius:", radius);
+// console.log("Circumference:", circumference.toFixed(2));
+// console.log("Area:", area.toFixed(2));
+
+
+//Problem-9
+
+
+// let noteBook =  45;
+// let pen =  15;
+// backpack = 850;
+// let subTotal = (noteBook * 3) + (pen * 2) + (backpack * 1);
+// let discount = subTotal * 0.1; // 10% discount
+// let afterDiscount = subTotal - discount;
+// console.log(afterDiscount);
+
+
+//Problem-10
+
+let number = 17;
+if (number % 2 === 0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
