3baad397469bbec4492c1b73e24746c146ae27ec
Mohammed Bakhar <mrbakhar8686@gmail.com>
2026-06-09 18:24:28 +0530

    day-4 assignment completed

---
+++ day_4/assignment.js
@@ -0,0 +1,62 @@
+// Question 1:
+
+// let userInput =prompt( "Enter the input");
+// let cleanInput = userInput.trim().toLowerCase();
+// console.log(userInput);
+// console.log(cleanInput);
+
+// Question 2:
+
+
+// let email ="       USER@EXAMPLE.COM       ";
+// let formattedEmail = email.trim().toLowerCase();
+// console.log(formattedEmail)
+
+
+// Question 3:
+
+
+// let sentence = "JavaScript is awesome and JavaScript is fun" ;
+// console.log(sentence.indexOf("awesome") );
+// console.log(sentence.indexOf("JavaScript") );
+// console.log(sentence.indexOf("Python") );
+
+
+// Question 4:
+
+
+// let message = "Welcome to coding class";
+// sentence = message.indexOf("coding");
+// if(sentence >= 0){
+// console.log("Found coding!");
+// }
+// else
+//     {console.log("Coding not found")};
+
+
+// Section 3:
+
+// Question 1:
+
+
+// let rawData = "   MESSY iNpuT  ";
+// let ProcesseedData = rawData.trim().toLowerCase();
+// console.log(rawData);
+// console.log(ProcesseedData);
+
+
+// Question 2:
+
+// let passWord = "  MyP@ssw0rd123  ";
+// let securePassword = passWord.trim().toUpperCase().slice(0,8);
+// console.log("securePassword:" ,securePassword);
+
+// section 4:
+
+//  Question 1:
+
+let  fullName = "Alexander Hamilton";
+let firstName = fullName.slice(0,9);
+let lastName = fullName.slice(10);
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
